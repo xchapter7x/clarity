11562300eaab23012cac4fb3b74db8a7a64f4d1b
John Calabrese <xchapter7x@gmail.com>
2019-03-09 18:56:05 -0500

    add step phrase structure choices

--- cmd/clarity/clarity_test.go
+++ cmd/clarity/clarity_test.go
@@ -19,18 +19,25 @@ func FeatureContext(s *godog.Suite) {
 	s.Step(`^pending "([^"]*)"$`, markPending)
 	s.Step(`^Terraform$`, match.Terraform)
 	s.Step(`^a "([^"]*)" of type "([^"]*)"$`, match.AOfType)
-	s.Step(`^attribute "([^"]*)" equals (\d+)$`, match.AttributeEqualsInt)
-	s.Step(`^attribute "([^"]*)" does not equal (\d+)$`, match.AttributeDoesNotEqualInt)
-	s.Step(`^attribute "([^"]*)" equals "([^"]*)"$`, match.AttributeEquals)
-	s.Step(`^attribute "([^"]*)" does not equal "([^"]*)"$`, match.AttributeDoesNotEqual)
+	s.Step(`^a "([^"]*)" of type "([^"]*)" named "([^"]*)"$`, match.AOfTypeNamed)
+
+	for _, phrasePrefix := range []string{
+		"the value of",
+		"attribute",
+	} {
+		s.Step(`^`+phrasePrefix+` "([^"]*)" equals (\d+)$`, match.AttributeEqualsInt)
+		s.Step(`^`+phrasePrefix+` "([^"]*)" does not equal (\d+)$`, match.AttributeDoesNotEqualInt)
+		s.Step(`^`+phrasePrefix+` "([^"]*)" equals "([^"]*)"$`, match.AttributeEquals)
+		s.Step(`^`+phrasePrefix+` "([^"]*)" does not equal "([^"]*)"$`, match.AttributeDoesNotEqual)
+		s.Step(`^`+phrasePrefix+` "([^"]*)" matches regex "([^"]*)"$`, match.AttributeRegex)
+		s.Step(`^`+phrasePrefix+` "([^"]*)" is greater than (\d+)$`, match.AttributeGreaterThan)
+		s.Step(`^`+phrasePrefix+` "([^"]*)" is less than (\d+)$`, match.AttributeLessThan)
+	}
+
 	s.Step(`^attribute "([^"]*)" exists$`, match.AttributeExists)
 	s.Step(`^it occurs at least (\d+) times$`, match.ItOccursAtLeastTimes)
 	s.Step(`^it occurs at most (\d+) times$`, match.ItOccursAtMostTimes)
 	s.Step(`^it occurs exactly (\d+) times$`, match.ItOccursExactlyTimes)
-	s.Step(`^attribute "([^"]*)" matches regex "([^"]*)"$`, match.AttributeRegex)
-	s.Step(`^a "([^"]*)" of type "([^"]*)" named "([^"]*)"$`, match.AOfTypeNamed)
-	s.Step(`^attribute "([^"]*)" is greater than (\d+)$`, match.AttributeGreaterThan)
-	s.Step(`^attribute "([^"]*)" is less than (\d+)$`, match.AttributeLessThan)
 	s.BeforeScenario(func(interface{}) {
 		match = matchers.NewMatch()
 	})
