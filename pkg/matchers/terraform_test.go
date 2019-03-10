52e24ae11aa1c6ea55dd7c207ae7e131305e9433
John Calabrese <xchapter7x@gmail.com>
2019-03-09 19:22:29 -0500

    more generic test builder function

--- pkg/matchers/terraform_test.go
+++ pkg/matchers/terraform_test.go
@@ -41,23 +41,19 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AOfType", func(t *testing.T) {
 		t.Run("should return a filtered list of definitions", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatch()))
 		})
 	})
 
 	t.Run("m.AttributeExists", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := &matchers.Match{}
-			err := m.ReadTerraform("testdata")
-			Expect(err).NotTo(HaveOccurred())
-			err = m.AOfType("google_compute_subnetwork", "resource")
-			Expect(err).NotTo(HaveOccurred())
+			m := resourceMatcherByType("google_compute_subnetwork")
 			Expect(m.AttributeExists("bbbbbasdf")).To(HaveOccurred())
 		})
 
 		t.Run("when there are matches", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeExists("name")).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatch()))
 		})
@@ -76,23 +72,23 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AttributeGreaterThan", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeGreaterThan("nadafield", 81)).To(HaveOccurred())
 		})
 
 		t.Run("when a valid attribute has non gt value", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeGreaterThan("port", 81)).To(HaveOccurred())
 		})
 
 		t.Run("when there are matches with a correct value", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeGreaterThan("port", 79)).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatchIntString()))
 		})
 
 		t.Run("when there are matches with a correct value and the attr value is a int type", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeGreaterThan("port", 79)).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatchInt()))
 		})
@@ -100,48 +96,48 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.ItOccursAtLeastTimes", func(t *testing.T) {
 		t.Run("when we dont have enough to satisfy at least", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.ItOccursAtLeastTimes(5)).To(HaveOccurred())
 		})
 
 		t.Run("when we have enough to satisfy at least", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.ItOccursAtLeastTimes(1)).NotTo(HaveOccurred())
 		})
 	})
 
 	t.Run("m.ItOccursAtMostTimes", func(t *testing.T) {
 		t.Run("when we have too many to satisfy at most", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.ItOccursAtMostTimes(0)).To(HaveOccurred())
 		})
 
 		t.Run("when we have enough to satisfy at most", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.ItOccursAtMostTimes(3)).NotTo(HaveOccurred())
 		})
 	})
 
 	t.Run("m.ItOccursExactlyTimes", func(t *testing.T) {
 		t.Run("when we dont have exactly the count", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.ItOccursExactlyTimes(2)).To(HaveOccurred())
 		})
 
 		t.Run("when we have exactly the count", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.ItOccursExactlyTimes(1)).NotTo(HaveOccurred())
 		})
 	})
 
 	t.Run("m.AttributeRegex", func(t *testing.T) {
 		t.Run("when we do not have a attr value which satisfies the regex", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeRegex("port", "abc")).To(HaveOccurred())
 		})
 
 		t.Run("when our attribute value satisfies the regex", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeRegex("port", "8*")).NotTo(HaveOccurred())
 		})
 
@@ -158,23 +154,23 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AttributeLessThan", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeLessThan("nadafield", 79)).To(HaveOccurred())
 		})
 
 		t.Run("when a valid attribute has non gt value", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeLessThan("port", 79)).To(HaveOccurred())
 		})
 
 		t.Run("when there are matches with a correct value", func(t *testing.T) {
-			m := blahBlahResourceMatcher()
+			m := resourceMatcherByType("blah_blah")
 			Expect(m.AttributeLessThan("port", 81)).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatchIntString()))
 		})
 
 		t.Run("when there are matches with a correct value and attr value is type int", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeLessThan("port", 81)).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatchInt()))
 		})
@@ -182,17 +178,17 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AttributeEqualsInt", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeEqualsInt("bbbbbasdf", 0)).To(HaveOccurred())
 		})
 
 		t.Run("when a valid attribute has non equal value", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeEqualsInt("port", 43)).To(HaveOccurred())
 		})
 
 		t.Run("when there are matches with a correct value", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeEqualsInt("port", 80)).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatchInt()))
 		})
@@ -200,17 +196,17 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AttributeDoesNotEqualInt", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeDoesNotEqualInt("bbbbbasdf", 0)).To(HaveOccurred())
 		})
 
 		t.Run("when a valid attribute has non equal value", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeDoesNotEqualInt("port", 80)).To(HaveOccurred())
 		})
 
 		t.Run("when there are matches with a correct value", func(t *testing.T) {
-			m := fooResourceMatcher()
+			m := resourceMatcherByType("foo")
 			Expect(m.AttributeDoesNotEqualInt("port", 81)).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatchInt()))
 		})
@@ -218,17 +214,17 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AttributeEquals", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeEquals("bbbbbasdf", "my-custom-network")).To(HaveOccurred())
 		})
 
 		t.Run("when a valid attribute has non equal value", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeEquals("name", "'wrong-name'")).To(HaveOccurred())
 		})
 
 		t.Run("when there are matches with a correct value", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeEquals("name", "my-custom-network")).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatch()))
 		})
@@ -236,18 +232,18 @@ func TestMatchers(t *testing.T) {
 
 	t.Run("m.AttributeDoesNotEqual", func(t *testing.T) {
 		t.Run("when there are no matches", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeDoesNotEqual("bbbbbasdf", "my-custom-network")).To(HaveOccurred())
 		})
 
 		t.Run("when a valid attribute has non equal value", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeDoesNotEqual("name", "wrong-name")).NotTo(HaveOccurred())
 			Expect(m.MatchingEntries).To(BeEquivalentTo(controlInstanceMatch()))
 		})
 
 		t.Run("when there are matches with a correct value", func(t *testing.T) {
-			m := googleComputeNetworkResourceMatcher()
+			m := resourceMatcherByType("google_compute_network")
 			Expect(m.AttributeDoesNotEqual("name", "my-custom-network")).To(HaveOccurred())
 		})
 	})
@@ -335,29 +331,11 @@ func multiTFControl() []matchers.HCLEntry {
 	}
 }
 
-func fooResourceMatcher() *matchers.Match {
+func resourceMatcherByType(resourceType string) *matchers.Match {
 	m := &matchers.Match{}
 	err := m.ReadTerraform("testdata")
 	Expect(err).NotTo(HaveOccurred())
-	err = m.AOfType("foo", "resource")
-	Expect(err).NotTo(HaveOccurred())
-	return m
-}
-
-func blahBlahResourceMatcher() *matchers.Match {
-	m := &matchers.Match{}
-	err := m.ReadTerraform("testdata")
-	Expect(err).NotTo(HaveOccurred())
-	err = m.AOfType("blah_blah", "resource")
-	Expect(err).NotTo(HaveOccurred())
-	return m
-}
-
-func googleComputeNetworkResourceMatcher() *matchers.Match {
-	m := &matchers.Match{}
-	err := m.ReadTerraform("testdata")
-	Expect(err).NotTo(HaveOccurred())
-	err = m.AOfType("google_compute_network", "resource")
+	err = m.AOfType(resourceType, "resource")
 	Expect(err).NotTo(HaveOccurred())
 	return m
 }
